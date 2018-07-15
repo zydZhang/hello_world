13cf2865918fe99641ff5c45580a30e120d8159d
andy <568321490@qq.com>
2018-07-15 11:57:02 +0800

    add vim txt

---
+++ vim.php
@@ -0,0 +1,15 @@
+<?php
+echo 'welcome to vim study';
+
+echo 'nice to meet you';
+
+//yy:复制一行
+//yy:复制一行, p:粘贴
+
+//u:撤销上一次操作      U:撤销当前行的所有修改
+// ctrl+r:对撤消的撤消
+
+//33G：跳转到33行
+//gg:文件头   G： 文件尾
+//set number:设置显示行号，set nonumber:关闭显示
+//:set hlsearch  高亮显示查找结果
